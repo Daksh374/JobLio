ec9f90eb8bbb0bf8e2e286523f96708353f42452
Daksh Yadav <dakshrao1111@gmail.com>
2026-04-05 16:42:03 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-lio.vercel.app/',
+    origin:'https://job-lio.vercel.app',
     credentials:true
 }
 
